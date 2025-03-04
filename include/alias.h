f6eb006b2d6176c6d6ae3bce7d61dcd407460526
SergantDornan <microsoftvis@gmail.com>
2025-03-04 22:04:37 +0300

    multiple terminal configs support

--- include/alias.h
+++ include/alias.h
@@ -4,7 +4,7 @@
 #include "BuilderFilework.h"
 const std::vector<std::string> configFiles = {getHomedir() + "/.bashrc", 
 	getHomedir() + "/.zshrc", getHomedir() + "/.config/fish/config.fish"};
-std::string findConfig();
+std::vector<std::string> findConfig();
 std::string getLine(const std::string&, const std::string&, const std::string&);
 void addAlias(const std::string&, const std::string&);
 void removeAlias(const std::string&, const std::string&);
--- source/alias.cpp
+++ source/alias.cpp
@@ -1,10 +1,11 @@
 #include "alias.h"
-std::string findConfig(){
+std::vector<std::string> findConfig(){
+	std::vector<std::string> v;
 	for(int i = 0; i < configFiles.size(); ++i){
 		if(exists(configFiles[i]))
-			return configFiles[i];
+			v.push_back(configFiles[i]);
 	}
-	return "-1";
+	return v;
 }
 std::string getLine(const std::string& config, const std::string& alias,
 	const std::string& cmd){
@@ -16,51 +17,55 @@ std::string getLine(const std::string& config, const std::string& alias,
 	return "-1";
 }
 void addAlias(const std::string& alias, const std::string& cmd){
-	std::string config = findConfig();
-	if(config == "-1"){
+	std::vector<std::string> config = findConfig();
+	if(config.size() == 0){
 		std::cout << "======================= ERROR =======================" << std::endl;
 		std::cout << "alias.cpp: addAlias: cannot find config file " << std::endl;
 		std::cout << "probably does not work for your system" << std::endl;
 		std::cout << "cannot add alias" << std::endl;
 		return;
 	}
-	std::string line = getLine(config, alias, cmd);
-	if(line == "-1"){
-		std::cout << "======================= ERROR =======================" << std::endl;
-		std::cout << "alias.cpp: addAlias: some error in: " << std::endl;
-		std::cout << "alias.cpp: getLine" << std::endl;
-		return;
+	for(int i = 0; i < config.size(); ++i){
+		std::string line = getLine(config[i], alias, cmd);
+		if(line == "-1"){
+			std::cout << "======================= ERROR =======================" << std::endl;
+			std::cout << "alias.cpp: addAlias: some error in: " << std::endl;
+			std::cout << "alias.cpp: getLine" << std::endl;
+			return;
+		}
+		std::ofstream bshrc(config[i], std::ios::app);
+    	bshrc << line << std::endl;
+    	bshrc.close();
 	}
-	std::ofstream bshrc(config, std::ios::app);
-    bshrc << line << std::endl;
-    bshrc.close();
 }
 void removeAlias(const std::string& alias, const std::string& cmd){
-	std::string config = findConfig();
-	if(config == "-1"){
+	std::vector<std::string> config = findConfig();
+	if(config.size() == 0){
 		std::cout << "======================= ERROR =======================" << std::endl;
 		std::cout << "alias.cpp: removeAlias: cannot find config file " << std::endl;
 		std::cout << "probably does not work for your system" << std::endl;
 		std::cout << "cannot remove alias" << std::endl;
 		return;
 	}
-	std::string line = getLine(config, alias, cmd);
-	if(line == "-1"){
-		std::cout << "======================= ERROR =======================" << std::endl;
-		std::cout << "alias.cpp: removeAlias: some error in: " << std::endl;
-		std::cout << "alias.cpp: getLine" << std::endl;
-		return;
+	for(int i = 0; i < config.size(); ++i){
+		std::string line = getLine(config[i], alias, cmd);
+		if(line == "-1"){
+			std::cout << "======================= ERROR =======================" << std::endl;
+			std::cout << "alias.cpp: removeAlias: some error in: " << std::endl;
+			std::cout << "alias.cpp: getLine" << std::endl;
+			return;
+		}
+    	std::string l;
+    	std::vector<std::string> v;
+    	std::ifstream file(config[i]);
+    	while(std::getline(file,l)){
+        	if(l != line)
+            	v.push_back(line);
+    	}
+    	file.close();
+    	std::ofstream newfile(config[i]);
+    	for(int i = 0; i < v.size(); ++i)
+        	newfile << v[i] << std::endl;
+    	newfile.close();
 	}
-    std::string l;
-    std::vector<std::string> v;
-    std::ifstream file(config);
-    while(std::getline(file,l)){
-        if(l != line)
-            v.push_back(line);
-    }
-    file.close();
-    std::ofstream newfile(config);
-    for(int i = 0; i < v.size(); ++i)
-        newfile << v[i] << std::endl;
-    newfile.close();
 }
